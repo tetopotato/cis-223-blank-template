6e6780380928f251018fdbbf80a983da58754cb9
Caleb Foss <calebfoss@gmail.com>
2024-02-29 14:06:31 -0600

    Add button demonstration

---
+++ script.js
@@ -0,0 +1,11 @@
+// Create a button element
+const button = document.createElement("button");
+
+// Create button text
+const buttonText = new Text("Click me!");
+
+// Put button text in button
+button.appendChild(buttonText);
+
+// Put button in document's body
+document.body.appendChild(button);
